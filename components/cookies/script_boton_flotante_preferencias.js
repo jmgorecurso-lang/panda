3a23ee236adcc1c310450e95a27f2bf02b82370a
jmgorecurso-lang <jmgorecurso@gmail.com>
2026-06-30 22:27:12 +0200

    Update script_boton_flotante_preferencias.js

--- components/cookies/script_boton_flotante_preferencias.js
+++ components/cookies/script_boton_flotante_preferencias.js
@@ -3,7 +3,7 @@ const floatingBtn = document.createElement('button');
 floatingBtn.innerHTML = '<img src="/access/favicon.ico">';
 floatingBtn.style.cssText = `
     position: fixed;
-    bottom: 20px;
+    bottom: 30px;
     left: 20px;
     width: 50px;
     height: 50px;
